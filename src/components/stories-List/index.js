5a2ff43ff885d84af5133a24b7c17e6ceb560682
Deeksha Amin <diya-deekshaa@CPU1595.local>
2020-10-08 17:13:54 +0530

    css updates

--- src/components/stories-List/index.js
+++ src/components/stories-List/index.js
@@ -5,28 +5,30 @@ import './index.scss';
 const StoriesList = ({ data }) => {
     const [s_issue, set_issue] = useState({});
     return (
-        <div className="stories_list container">
-            {(!s_issue || Object.keys(s_issue).length == 0) &&
-                <>
-               <div className="row head-section">
-                <div className="col-md-8 col-sm-6">
-                    <ul className="nav nav-tabs tab">
-                        <li className="nav-item"><a className="nav-link active" href="">Pending</a></li>
-                        <li className="nav-item"><a className="nav-link" href="">Ready</a></li>
-                    </ul>
+        <div className="col-md-8">
+            <div className="stories_list boxbg container">
+                {(!s_issue || Object.keys(s_issue).length == 0) &&
+                    <>
+                <div className="row heading">
+                    <div className="col-md-8 col-sm-6">
+                        <ul className="nav nav-tabs tab">
+                            <li className="nav-item"><a className="nav-link active" href="">Pending</a></li>
+                            <li className="nav-item"><a className="nav-link" href="">Ready</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-md-4 col-sm-6">
+                        <button className="sync"><i class="fa fa-spin fa-refresh" aria-hidden="true"></i></button>
+                    </div>
                 </div>
-                <div className="col-md-4 col-sm-6">
-                    <button className="sync"><i class="fa fa-spin fa-refresh" aria-hidden="true"></i></button>
-                </div>
-            </div>
-            <ul className="list-section">
-            {Array.isArray(data) && data.length > 0 && 
-            data.map((o,i)=><li>{o.id}--- {o.key}</li>)}
-            </ul>
-                </>
-            }
-            {s_issue && Object.keys(s_issue).length > 0 && <IssueDescription issue={s_issue} set_issue={set_issue}/>}
+                <ul className="list-section">
+                {Array.isArray(data) && data.length > 0 && 
+                data.map((o,i)=><li>{o.id}--- {o.key}</li>)}
+                </ul>
+                    </>
+                }
+                {s_issue && Object.keys(s_issue).length > 0 && <IssueDescription issue={s_issue} set_issue={set_issue}/>}
 
+            </div>
         </div>
     );
 }
