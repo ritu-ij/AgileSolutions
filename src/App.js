4cd7c26f80a1b4d7525b925e14ae7e27d5b116ee
nency.h <nency.h@endurance.com>
2020-10-08 11:46:44 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createBrowserHistory } from 'history';
 import { BrowserRouter, Route, } from 'react-router-dom';
 import PlanningPoker from './pages/planning-pocker/Dashboard';
 import constants from './helpers/constants';
-import {getAuth, getUserData } from './api/apiCalls';
+import {getAuth, getUserData , getAllProject,getAccessableResource} from './api/apiCalls';
 import './App.css';
 
 function App() {
+  const [ project , setProject ] = useState('');
   const history = createBrowserHistory();
   const auth_token = localStorage.getItem('auth_token');
   let search = window.location.search;
   let params = new URLSearchParams(search);
   const code = params.get('code');
-  if (!auth_token && !code) {
-    window.location.href = constants.REST_API_authorization_URL;
-  } else{
-    if(!auth_token || auth_token == 'undefined'){
-      getAuth(code)
+  useEffect(()=>{
+    if (!auth_token && !code) {
+      window.location.href = constants.REST_API_authorization_URL;
     } else{
-      let user_data = localStorage.getItem('user_details')
-      if(!user_data || user_data == 'undefined'){
-        getUserData()
+      if(!auth_token || auth_token == 'undefined'){
+        getAuth(code).then(()=>{
+          getUserData();
+          getAccessableResource().then(()=>{
+            getAllProject();
+          });
+        })
+      } else{
+        let user_data = localStorage.getItem('user_details')
+        if(!user_data || user_data == 'undefined'){
+          getUserData();
+          getAccessableResource().then(()=>{
+            getAllProject().then((project)=>{
+              setProject(project);
+            });
+          });
+        } else {
+          getAllProject().then((project)=>{
+            setProject(project);
+          });
+        }
       }
     }
-  }
+  },[])
+  
   return (
     <BrowserRouter history={history}>
-      <Route path="/" component={PlanningPoker} />
+      <Route path="/" component={() => <PlanningPoker project={project}/>} />
     </BrowserRouter>
   )
 }
--- src/pages/planning-pocker/Dashboard/index.js
+++ src/pages/planning-pocker/Dashboard/index.js
@@ -1,28 +1,73 @@
-import React,{PureComponent} from 'react';
+import React, { useEffect, useState } from 'react';
 import Search from '../../../components/Search';
 import StoriesList from '../../../components/stories-List';
 import PeopleList from '../../../components/people-list';
-import {getAllProject} from '../../../api/apiCalls';
 import './index.scss';
+// import data from '../../../helpers/constants';
+import {getPeopleInProject, getgroups, getProjectPeople, searchIsuue} from '../../../api/apiCalls';
 
-class Dashboard extends PureComponent {
-    componentDidMount(){
-        // getAllProject('backlog');
+const Dashboard = ({ project }) => {
+    const [s_Project, setS_Project] = useState({});
+    const [people,setPeople] = useState([]);
+    const [issue,setIssue] = useState({});
+
+    useEffect(() => {
+        if (Array.isArray(project) && project.length > 0) {
+            setS_Project(project[0]);
+            getPeopleList();
+            getIssueList(project[0]);
+            
+        }
+    }, [project])
+
+    useEffect(()=>{
+        getPeopleInProject(s_Project.id).then((data)=>{
+            console.log(data);
+        })
+        getgroups();
+    },[s_Project])
+
+
+    const getiisue=  ()=>{
+        getgroups().then((data)=>{
+            console.log(data);
+        })
     }
-    state = {  }
-    render() { 
-        return (
-            <div className="main-div">
-                <div className="container-search">
-                    <h3>Agile Agents</h3><Search/>
-                </div>
-                <div className="container-stories">
-                    <StoriesList/>
-                    <PeopleList/>
-                </div>
-            </div>  
-        );
+
+    const getPeopleList = ()=>{
+        getProjectPeople(s_Project.id).then((data)=>{
+            setPeople(data)
+        })
     }
+
+    const getIssueList = (project) =>{
+        searchIsuue(project).then((data)=>{
+            setIssue(data);
+        });
+    }
+
+    return (
+        <div className="main-div">
+            {
+                Array.isArray(project) && project.length > 0 &&
+                <div>selcted project:
+                    <select defaultValue={s_Project.name} onChange={(event) => { setS_Project(project[event.target.value]) }}>
+                        {project.map((data, i) => <option value={i}>{data.name}</option>)}
+                    </select>
+                </div>
+            }
+            <button onClick={getiisue}>get issue</button>
+            <button onClick={getPeopleList}>get project details</button>
+
+            <div className="container-search">
+                <h3>Agile Agents</h3><Search />
+            </div>
+            <div className="container-stories">
+               {issue && issue.issues && <StoriesList data={issue.issues || []}/>}
+                <PeopleList data={people}/>
+            </div>
+        </div>
+    );
 }
- 
+
 export default Dashboard;
