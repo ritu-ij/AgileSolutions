cb72acc1afafb27d2453b918f7fda0af33ff502e
nency.h <nency.h@endurance.com>
2020-10-08 16:37:54 +0530

    changes

--- src/components/stories-List/index.js
+++ src/components/stories-List/index.js
@@ -4,6 +4,22 @@ import './index.scss';
 
 const StoriesList = ({ data }) => {
     const [s_issue, set_issue] = useState({});
+    const [storyType, setSToryType] = useState("pending");
+    const showSTory = (points)=>{
+        if(storyType == 'pending'){
+            if(points && points > 0){
+                return false;
+            } else{
+                return true;
+            }
+        }else{
+            if(points && points > 0){
+                return true;
+            } else{
+                return false;
+            }
+        }
+    }
     return (
         <div className="stories_list container">
             {(!s_issue || Object.keys(s_issue).length == 0) &&
@@ -11,8 +27,8 @@ const StoriesList = ({ data }) => {
                <div className="row head-section">
                 <div className="col-md-8 col-sm-6">
                     <ul className="tab">
-                        <li className="selected"><a href="">Pending</a></li>
-                        <li><a href="">Ready</a></li>
+                        <li className={storyType =='pending'?'selected':''} onClick={()=>setSToryType('pending')}><a >Pending</a></li>
+                        <li className={storyType =='ready'?'selected':''} onClick={()=>setSToryType('ready')}><a >Ready</a></li>
                     </ul>
                 </div>
                 <div className="col-md-4 col-sm-6">
@@ -21,7 +37,7 @@ const StoriesList = ({ data }) => {
             </div>
             <ul className="list-section">
             {Array.isArray(data) && data.length > 0 && 
-            data.map((o,i)=><li onClick={()=>{console.log('test');set_issue(o)}}>{o.id}--- {o.key}</li>)}
+                data.map((o,i)=>showSTory(o.fields.customfield_10032) && <li onClick={()=>{set_issue(o)}} key={o.id}>{o.id}--- {o.key}---points:{o.fields.customfield_10032}</li>)}
             </ul>
                 </>
             }
--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -8,7 +8,7 @@ const data = {
     },
     "BaseURL": "https://praveen-testing.atlassian.net/"
 }
-data.REST_API_authorization_URL = `https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=${data.client_id}&scope=read%3Ame%20read%3Ajira-work%20manage%3Ajira-project%20manage%3Ajira-configuration%20manage%3Ajira-data-provider%20read%3Ajira-user%20write%3Ajira-work&redirect_uri=${data.Callback_URL}&state=${data.YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent`
+data.REST_API_authorization_URL = `https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=${data.client_id}&scope=read%3Ame%20read%3Ajira-work%20manage%3Ajira-project%20manage%3Ajira-configuration%20manage%3Ajira-data-provider%20read%3Ajira-user%20write%3Ajira-work%3Areport:personal-data%3Awrite:servicedesk-request%3Aread:servicedesk-request%3Amanage:servicedesk-customer&redirect_uri=${data.Callback_URL}&state=${data.YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent`
 
 export default data;
 
