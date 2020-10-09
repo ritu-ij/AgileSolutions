75a36cfa1c6078a7f64ad31088e7f2c1441525b0
Deeksha Amin <diya-deekshaa@CPU1595.local>
2020-10-09 13:59:07 +0530

    updated checkbox image

--- src/pages/retrospective/Dashboard/index.jsx
+++ src/pages/retrospective/Dashboard/index.jsx
@@ -27,7 +27,7 @@ const RetrospectivePage = (props) => {
                     <Tracker content={["improvement point #1", "improvement point #2", "improvement point #3", "improvement point #4", "improvement point #5", "improvement point #6", "improvement point #7", "improvement point #8"]} head="Continuous Improvement Tracker" defaultChecked={false} />
                 </section>
                 <section class="accomplishment-tracker">
-                    <Tracker content={["achieved point #1", "achieved point #2", "achieved point #3", "achieved point #4", "achieved point #5", "achieved point #6", "achieved point #7", "achieved point #8"]} head="Achievement" defaultChecked={true} />
+                    <Tracker content={["improvement achieved point #1", "improvement achieved point #2", "improvement achieved point #3", "improvement achieved point #4", "improvement achieved point #5", "improvement achieved point #6", "improvement achieved point #7", "improvement achieved point #8"]} head="Achievement" defaultChecked={true} />
                 </section>
             </div>
         </div>
